42d2bd00ee781d9f55bbbac7667708a4a84f3c59
Rajabov Bobokhon <rboboxon6055@gmail.com>
2021-06-18 22:11:30 +0900

    alan-ai added

--- src/Components/CardData/index.jsx
+++ src/Components/CardData/index.jsx
@@ -5,12 +5,23 @@ import Footer from '../Footer'
 import HeaderCoin from '../Header'
 import TextTicker from '../TextTicker'
 import TotalPrice from '../TotalPrice'
+import alanBtn from "@alan-ai/alan-sdk-web";
 
 import{Container,SpinnerLoading} from  "./style"
+const alanKey = "a1e879e5210af325e788384d2bd2b70b2e956eca572e1d8b807a3e2338fdd0dc/stage";
 const CardData = () => {
    const[coins,setCoins] = useState([])
    const[isLoading,setIsLoading] = useState(false)
-   
+   useEffect(() => {
+      alanBtn({
+        key: alanKey,
+        onCommand: ({ command, articles }) => {
+          if (command === "newHeadlines") {
+            console.log(articles);
+          }
+        },
+      });
+    }, []);
 
    useEffect(() =>{
       const URL = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false'
